7603bbe2898bb04382212e83cafa5a928517ba26
Abhishek Kumar <abhishekjha12017@gmail.com>
2025-04-23 16:26:38 +0530

    get user input and add make_descision function

--- main.py
+++ main.py
@@ -3,7 +3,23 @@ import random
 #choices
 choices = ["Rock","Paper","Scissors"]
 
+#get user input
+user = input("Enter your choice: ")
+
 #computer choice
 computer = random.choice(choices)
+print(F"computer_coice: {computer}")
 
-print(computer)
+#This fucnction decide who win the game
+def make_decision(computer,user):
+    if user == "Rock" and computer == "Scissors":
+        print("You win!")
+    elif user == "Scissors" and computer == "Paper":
+        print("You win!")
+    elif user == "Paper" and computer == "Rock":
+        print("You win!")
+    else:
+        print("Computer win!")
+
+#Call the function
+make_decision(computer,user)
